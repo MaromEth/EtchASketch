ce2c8e6354ac9755dc0df7f66327915f0feb27c5
Ethan Marom <ethanmarom5@gmail.com>
2022-08-12 13:34:38 -0400

    Add functionality that changes cell colour once clicked

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var tag = document.createElement("div");
 
-var element = document.getElementById("new");
+var element = document.getElementById("container");
 
 
 for(let i = 1; i<17; i++){
@@ -13,11 +13,27 @@ for(let i = 1; i<17; i++){
     for(let j = 1; j<17; j++){
         var tags = document.createElement("div");
         tags.setAttribute('id', `${j}`)
+        tags.setAttribute('tag', `cell`)
+        //tags.setAttribute('selected', 'false')
+        tags.setAttribute('parent', `${i}`)
         elements.appendChild(tags)
         elements.removeAttribute("id")
         console.log(elements)
         
-       // removeElement.removeAttribute('tag')
-    }
+     }
 }
+var cells = document.querySelectorAll('tag')
+addEventListener('click', function(e) {
+    //cells.setAttribute('selected', 'true')
+    //tags.appendChild(cells)
+//console.log(`selected ${cells.id}`)
+//console.log(e.path[0])
+    const selectedCell = e.path[0]
+    console.log(selectedCell)
+    //selectedCell.setAttribute('selected', 'true')
+    selectedCell.toggleAttribute('selected')
+//const cellClicked =  document.querySelector()
+//cells.classList
+    //this.toggleAttribute('selected')
+})
 
