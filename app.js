050a5c8e596e545a4a8934d0e9299d6705d64944
Ethan Marom <ethanmarom5@gmail.com>
2022-08-15 15:04:37 -0400

    Add ability for user to input grid size as well as fix auto sizing issues

--- app.js
+++ app.js
@@ -1,39 +1,57 @@
 var tag = document.createElement("div");
 
 var element = document.getElementById("container");
-
-
-for(let i = 1; i<17; i++){
+var numberOfCells = window.prompt("enter the amount of rows you would like (max of 100)")
+let cellHeight = 960/numberOfCells;
+//console.log(cellHeight)
+let cellHeightString = `${cellHeight}px`
+//console.log(typeof cellHeightString);
+var cellIdentifier = document.querySelectorAll('cell');
+console.log(cellIdentifier)
+//cellIdentifier.style.height = cellHeightString;
+for(let i = 1; i<numberOfCells; i++){
     
     var tag = document.createElement("div");
     tag.setAttribute('id', `row`)
-    tag.setAttribute('tag', `${i}`)
+    tag.setAttribute('tag', `parent`)
+    tag.setAttribute('height',`${cellHeightString}` )
     element.appendChild(tag);
     var elements = document.getElementById(`row`)
-    for(let j = 1; j<17; j++){
+    for(let j = 1; j<numberOfCells; j++){
         var tags = document.createElement("div");
-        tags.setAttribute('id', `${j}`)
-        tags.setAttribute('tag', `cell`)
-        //tags.setAttribute('selected', 'false')
+        tags.setAttribute('id', `cell`)
+        tags.setAttribute('cell', `yes`)
         tags.setAttribute('parent', `${i}`)
+        tags.setAttribute('tag', `cell`)
+        tags.setAttribute('class', 'cell')
+        tags.style.height = cellHeightString;
+        tags.style.width = cellHeightString;
         elements.appendChild(tags)
         elements.removeAttribute("id")
         console.log(elements)
         
      }
 }
-var cells = document.querySelectorAll('tag')
-addEventListener('click', function(e) {
-    //cells.setAttribute('selected', 'true')
-    //tags.appendChild(cells)
-//console.log(`selected ${cells.id}`)
-//console.log(e.path[0])
+var cells = document.querySelectorAll('parent')
+    addEventListener('mouseover', function(e) {
     const selectedCell = e.path[0]
-    console.log(selectedCell)
-    //selectedCell.setAttribute('selected', 'true')
-    selectedCell.toggleAttribute('selected')
-//const cellClicked =  document.querySelector()
-//cells.classList
-    //this.toggleAttribute('selected')
+    //console.log(selectedCell.getAttribute('parent'))
+    console.log(typeof selectedCell.getAttribute('parent'))
+    if(typeof selectedCell.getAttribute('parent') === 'string'){
+        selectedCell.setAttribute('selected', 'true')
+        console.log(selectedCell)
+    }
+    
 })
+var button = document.getElementById("button");
 
+
+button.addEventListener('click', function(e) {
+    var allCells = document.getElementsByClassName('cell');
+    //allCells.removeAttribute('selected')
+    for(var i = 0; i < allCells.length; i++){
+        allCells[i].removeAttribute("selected"); 
+     }
+})
+//document.getElementById('cell').style.height = cellHeightString;
+//let cellSize = 
